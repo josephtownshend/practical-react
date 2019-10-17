910fbb0fa352245600586783ee0a29eccc8ede89
joseph townshend <joetownshend@mac.com>
2019-10-17 20:29:20 +0100

    feat: can toggle counter and keep state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import ImageSlider from './components/ImageSlider';
 import Counter from './components/Counter';
-import Header from './components/Header';
 
 
 class App extends Component {
 
   state = {
     visible: true,
-    whichComponentToShow: "ImageSlider"
   };
 
   render() {
-      return (
-          <div className="App">
-          {this.state.visible ? <Counter /> : null}
-          <button
-            onClick={() => {
-              this.setState({ visible: !this.state.visible });
-            }}
-          >
-            toggle counter
-          </button>
+    return (
+      <div className="App">
+        <div className={this.state.visible ? "visible" : "hidden"}>
+          <Counter />
         </div>
-      );
+        <button
+          onClick={() => {
+            this.setState({ visible: !this.state.visible });
+          }}
+        >
+          toggle counter
+        </button>
+      </div>
+    );
   }
 }
-
 export default App;
