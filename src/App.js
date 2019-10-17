73f604e31fad105927d07665fd5e62fa2b45fc25
joseph townshend <joetownshend@mac.com>
2019-10-17 15:15:37 +0100

    feat: conditionally rendering components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import ImageSlider from './components/imageSlider';
+import ImageSlider from './components/ImageSlider';
+import Counter from './components/Counter';
 
 
 class App extends Component {
 
   state = {
-    visible: true
-  }
-
-  add(a, b) {
-    return a + b;
-  }
+    visible: true,
+    whichComponentToShow: "ImageSlider"
+  };
 
   render() {
-
-    const buttonText = this.state.visible ? 'hide' : 'show';
-    const slider = this.state.visible ? (
-      <ImageSlider />
-    ) : (
-      <div>I am hidden!</div>
-    );
-
-    return (
-      <div className="App">
-      {slider}
-        <button
-          onClick={() => {
-            this.setState({visible: !this.state.visible})
-          }}
-        >
-          {buttonText}
-        </button>
-      </div>
-    );
+    if (!this.state.whichComponentToShow === "ImageSlider") {
+      return (
+          <div className="App">
+          <ImageSlider />
+          <button
+            onClick={() => {
+              this.setState({whichComponentToShow: "Counter"})
+            }}
+          >
+            show counter
+          </button>
+        </div>
+      );
+    } else if (this.state.whichComponentToShow === "Counter") {
+      return (
+          <div className="App">
+          <Counter />
+          <button>show header</button>
+        </div>
+      )
+    }
+    return null;
   }
 }
 
