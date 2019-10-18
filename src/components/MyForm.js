02658c1cafada3c524060ff9a0fb59e5d041d442
joseph townshend <joetownshend@mac.com>
2019-10-18 14:36:40 +0100

    feat: text area saving state

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -3,17 +3,23 @@ import React from 'react';
 export default class MyForm extends React.Component {
 
   state = {
-    name: ''
+    name: '',
+    favePet: ''
   }
 
   handleChange = (event) => {
     this.setState({ name: event.target.value });
   };
 
+  handleChangeFavePet = (event) => {
+    this.setState({ favePet: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <input value={this.state.name} onChange={this.handleChange} />
+        <textarea value={this.state.favePet} onChange={this.handleChangeFavePet} />
       </div>
     );
   }
