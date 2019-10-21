b9002735baed1319d1b9efae9463665f3d0dcc85
joseph townshend <joetownshend@mac.com>
2019-10-21 14:58:47 +0100

    feat: form working with conditional errors

--- src/components/ValidationForm.js
+++ src/components/ValidationForm.js
@@ -6,11 +6,66 @@ export default class ValidationForm extends React.Component {
     name: '',
     email: '',
     password: '',
-    nameError: 'name is empty',
+    nameError: '',
     emailError: '',
     passwordError: ''
   };
 
+  handleChange = event => {
+    const isCheckbox = event.target.type === 'checkbox';
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
+  };
 
-  
+  handlesubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+        <input
+          name='name'
+          placeholder='name'
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <div style={{ fontSize: 12, color: 'red' }}>
+          {this.state.nameError}
+        </div>
+      </div>
+      <div>
+        <input
+            name='email'
+            placeholder='email'
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: 'red' }}>
+            {this.state.emailError}
+        </div>
+        </div>
+        <div>
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            value={this.state.pasword}
+            onChange={this.state.handleChange}
+            />
+            <div style={{ fontSize: 12, color: 'red' }}>
+              {this.state.passwordError}
+          </div>
+        </div>
+        <button type='submit'>submit</button>
+      </form>
+    )
+  }
+
+
 };
