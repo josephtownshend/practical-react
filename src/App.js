22370f779bbba03fa642345215315a86330c3dab
joseph townshend <joetownshend@mac.com>
2019-10-21 14:45:25 +0100

    setup: validationFrom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import MyForm from './components/MyForm';
+import ValidationForm from './components/ValidationForm';
 
 
 class App extends Component {
 
-  state = {
-    visible: true,
-  };
-
   render() {
     return (
       <div className="App">
-            <MyForm />
+            <ValidationForm />
       </div>
     );
   }
---
+++ src/components/ValidationForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class ValidationForm extends React.Component {
+
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    nameError: 'name is empty',
+    emailError: '',
+    passwordError: ''
+  };
+
+
+  
+};
